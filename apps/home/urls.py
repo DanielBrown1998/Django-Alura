77fb0ae64adc869ba7df082404eb3d29e9ee7971
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-03-11 03:31:27 -0300

    feat: add edicao de fotografia

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -8,10 +8,10 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('imagem/<int:item_id>/', views.image, name='imagem'),
+    path('imagem/<int:id>/', views.image, name='imagem'),
     path('tag/<str:categoria>/', views.tag, name='tag'),
     path('buscar/', views.buscar, name='buscar'),
     path('nova-imagem/', views.nova_imagem, name='nova-imagem'),
-    path('editar-imagem/<int:item_id>/', views.editar_imagem, name='editar-imagem'),
-    path('deletar-imagem/<int:item_id>/', views.deletar_imagem, name='deletar-imagem'),
+    path('editar-imagem/<int:id>/', views.editar_imagem, name='editar-imagem'),
+    path('deletar-imagem/<int:id>/', views.deletar_imagem, name='deletar-imagem'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from apps.home.models import Fotografia
 from apps.home.forms import FotografiaForm
@@ -20,11 +20,12 @@ def home(request):
     return render(request, 'index.html', context=context)
 
 @login_required(login_url='usuarios:login')
-def image(request, item_id):
-    image = get_object_or_404(Fotografia, pk=item_id)
+def image(request, id:int):
+    image = get_object_or_404(Fotografia, pk=id)
     context = {
         'title': f'{image.nome}',
         'item': image,
+        "item_id": id
     }
     return render(request, 'home/imagem.html', context=context)
 
@@ -82,10 +83,30 @@ def nova_imagem(request):
     return render(request, 'home/nova_imagem.html', context=context)
 
 @login_required(login_url='usuarios:login')
-def editar_imagem(request, id: int):
-    return render(request, 'home/editar_imagem.html')
+def editar_imagem(request, id:int):
+
+    fotografia = Fotografia.objects.get(pk=id) 
+    forms = FotografiaForm(instance=fotografia)
+
+    context = {
+        'title': 'Editar Imagem',
+        'form': forms,
+        'foto_id': id
+    }
+
+    if request.method == "POST":
+        forms = FotografiaForm(request.POST, request.FILES, instance=Fotografia)
+        if forms.is_valid():
+            forms.save()
+            msg = 'Imagem editada com sucesso!'
+            success(request, msg)
+            return render(request, 'index.html')
+        else:
+            error(request, 'Erro ao editar a imagem!')
+            return render(request, 'home/editar_imagem.html', context)
+    
+    return render(request, 'home/editar_imagem.html', context)
 
 @login_required(login_url='usuarios:login')
-def deletar_imagem(request, id: int):
-    return render(request, 'home/deletar_imagem.html')
-
+def deletar_imagem(request, id:int):
+    return redirect("home:home")
